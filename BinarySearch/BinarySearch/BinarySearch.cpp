1db67432ac7e9cdd216e342ddf70ca2c219dbb99
hoppiyoppi <joshuaevison@btinternet.com>
2020-01-22 22:25:31 +0000

    First version of my binary search

---
+++ BinarySearch/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,52 @@
+﻿#include "BinarySearch.h"
+
+static int BinarySearch(int target, int numbers[], int length)
+{
+	int* a = NULL;
+	int count = 0;
+
+	std::cout << length;
+	if (numbers[length / 2] == target)
+	{
+		std::cout << "Found " << numbers[length / 2];
+		delete [] a;
+		a = NULL;
+		return 0;
+	}
+	else if (numbers[length / 2] < target)
+	{
+		std::cout << "less than target";
+		a = new int[length- (length / 2)];
+		for (int i = 0; i < length-(length/2); i++)
+		{
+			a[i] = numbers[(length/2)+i];
+			count++;
+		}
+		length = count;
+	}
+	else if (numbers[length / 2] > target)
+	{
+		std::cout << "greater than target";
+		a = new int[length - (length / 2)];
+		for (int i = 0; i < length - (length / 2); i++)
+		{
+			a[i] = numbers[i];
+			count++;
+		}
+		length = count;
+	}
+	BinarySearch(target, a,length);
+}
+
+int main()
+{
+	int target = 15;
+	int numbers[10000];
+	for (int i = 0; i <= 9999; i++)
+	{
+		numbers[i] = i;
+	}
+	int length = sizeof(numbers) / sizeof(int);
+	BinarySearch(target, numbers, length);
+	return 0;
+}
